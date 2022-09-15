68cf587c03867f86d464c3eea36d0f8bd886ea6c
JgregoriBb <33870558+JgregoriBb@users.noreply.github.com>
2022-09-15 11:42:28 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# Imports
+# Change to add explicit imports from the helper classes.
+from script_helper import *
+
+def main():
+  
+  #your code goes here
+  pass
+
+if __name__ == "__main__":
+    main()
