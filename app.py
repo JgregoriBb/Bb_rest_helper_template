e0828de6415fef4728fed3ab4e3c2a4b52adfbae
JgregoriBb <33870558+JgregoriBb@users.noreply.github.com>
2022-09-19 11:21:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# Before adding any code or configurations make sure to change 
+# "gitignore" to ".gitignore" to make sure that unwanted config
+# and working files are not uploaded to the repository
+
 # Imports
 from script_helper import Helper
 # Other imports
